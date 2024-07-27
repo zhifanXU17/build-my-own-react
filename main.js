e10b4064d6f31ca3787cc5c4b782f3e9809aebbc
melo <zhifanXu17@gmail.com>
2024-07-27 14:41:35 +0800

    feat: createElement API

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+import { createElement } from './micro-react';
 
+const element = createElement(
+  'h1',
+  { id: 'title', class: 'hello' },
+  'hello-world',
+  createElement('h2')
+);
+
+console.log(element);
