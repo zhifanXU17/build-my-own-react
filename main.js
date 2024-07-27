86ad54f4f0d1dd17503c5a9cb982a2f3944ae9a0
melo <zhifanXu17@gmail.com>
2024-07-27 15:09:22 +0800

    feat:render API

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-import { createElement } from './micro-react';
+import { createElement, render } from './micro-react';
 
 const element = createElement(
   'h1',
-  { id: 'title', class: 'hello' },
-  'hello-world',
-  createElement('h2')
+  { id: 'title', style: 'background:blue' },
+  'Hello World',
+  createElement(
+    'a',
+    {
+      href: 'https://www.bilibili.com',
+      style: 'background: red',
+    },
+    'bilibili'
+  )
 );
+
+const container = document.getElementById('root');
+
+render(element, container);
 
 console.log(element);
